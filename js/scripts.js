06360b7286a301d2c06763b2b3c9b1d6c010e6c7
Chris Carr and Mark Fisher <chris@>
2017-02-13 13:12:09 -0800

    add the subtract, multiply, and divide functions, as well as a few other functions that dont belong in a calculator, such as bmi and temperature conversion

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,50 @@ var add = function(number1, number2) {
   return number1 + number2;
 };
 
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+var bmi = function(weight, height) {
+	return (weight / Math.pow(height, 2)) * 703;
+}
+//var number1 = parseInt(prompt("Enter weight in pounds:"));
+//var number2 = parseInt(prompt("Enter height in inches:"));
+//alert(bmi(number1, number2));
+
+
+var tempConvert = function(degree, isCelsius){
+	if(isCelsius === false){
+  	//then it's fahrenheit
+    var result = (degree-32)*5/9;
+  } else{
+  	//then it's Celsius
+    var result = (9*degree/5) +32;
+  }
+  return result;
+}
+//alert(tempConvert(71, false));
+//alert(tempConvert(0, true));
+
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 
-alert(add(number1, number2));
+var resultAdd = add(number1, number2);
+alert(resultAdd);
+
+var resultSubtract = subtract(number1, number2);
+alert(resultSubtract);
+
+var resultMultiply = multiply(number1, number2);
+alert(resultMultiply);
+
+var resultDivide = divide(number1, number2);
+alert(resultDivide);
