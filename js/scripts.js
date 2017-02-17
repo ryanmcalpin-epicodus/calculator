541d243d1963e1247570c2bc544e60d3142cb41d
Christian Martinez and Ryan McAlpin <christian@>
2017-02-16 16:15:21 -0800

    consolidate operator forms

--- js/scripts.js
+++ js/scripts.js
@@ -16,31 +16,22 @@ var divide = function(number1, number2) {
 
 
 $(document).ready(function() {
-  $("#add").submit(function(){
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    var result = add(number1, number2);
-    $("#output").text(result);
-    event.preventDefault();
-  });
-  $("#subtract").submit(function(){
-    var number1 = parseInt($("#subtract1").val());
-    var number2 = parseInt($("#subtract2").val());
-    var result = subtract(number1, number2);
-    $("#output").text(result);
-    event.preventDefault();
-  });
-  $("#multiply").submit(function(){
-    var number1 = parseInt($("#multiply1").val());
-    var number2 = parseInt($("#multiply2").val());
-    var result = multiply(number1, number2);
-    $("#output").text(result);
-    event.preventDefault();
-  });
-  $("#divide").submit(function(){
-    var number1 = parseInt($("#divide1").val());
-    var number2 = parseInt($("#divide2").val());
-    var result = divide(number1, number2);
+  $("#calculator").submit(function(){
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply"){
+      result = multiply(number1, number2);
+    } else {
+      result = divide(number1, number2);
+    }
+
+    
     $("#output").text(result);
     event.preventDefault();
   });
